53b3ad8905b5d0261c30b8239e4a000e574e0117
pupungeggang2 <kyongkyong289@outlook.kr>
2023-09-26 19:03:52 +0900

    0.0.1

---
+++ Card/Script/Var.js
@@ -0,0 +1,24 @@
+let canvas
+let context
+
+let gameInstance
+let gameFrameCurrent
+let gameFramePrevious
+let gameTime
+let delta
+
+let scene = 'Title'
+let state = ''
+let pause = false
+
+let player = {
+    
+}
+
+let field = {
+
+}
+
+let game = {
+
+}
