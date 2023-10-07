d5c35152ba7e7554cb49f1cf6c181cfba2324124
pupungeggang4 <kyongkyong289@gmail.com>
2023-10-08 01:37:53 +0900

    0.0.0

--- Card/Script/Main.js
+++ Card/Script/Main.js
@@ -5,10 +5,44 @@ window.oncontextmenu = rightClick
 function main() {
     canvas = document.getElemenyById('Screen')
     context = canvas.getContext('2d')
+
+    window.addEventListener('mouseup', mouseUp, false)
+    window.addEventListener('keydown', keyDown, false)
+    window.addEventListener('keyup', keyUp, false)
+
+    gameFrameCurrent = Date.now()
+    gameFramePrevious = Date.now() - 16
+
+    imageLoad()
+    gameInstance = requestAnimationFrame(loop)
+}
+
+function loop() {
+    gameFrameCurrent = Date.now()
+    gameFramePrevious = Date.now() - 16
+
+    gameInstance = requestAnimationFrame(loop)
+}
+
+function mouseUp(event) {
+    let canvasRect = canvas.getBoundingClientRect()
+    let x = event.clientX - canvasRect.left
+    let y = event.clientY - canvasRect.top
+    let button = event.button
+}
+
+function keyDown(event) {
+    let key = event.button
+}
+
+function keyUp(event) {
+    let key = event.button
 }
 
 function errorHandle(err, url, line, column, obj) {
-
+    if (obj != null) {
+        cancelAnimationFrame(gameInstance)
+    }
 }
 
 function rightClick() {
